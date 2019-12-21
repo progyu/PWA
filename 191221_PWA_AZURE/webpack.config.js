8232ba3081bccee3a0a33d0cfcfce2bebc97d5d3
progyu <rocket7101@gmail.com>
2019-12-21 15:43:19 +0900

    add manifest

--- 191221_PWA_AZURE/webpack.config.js
+++ 191221_PWA_AZURE/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: "./src/index.js", // webpack의 빌드 타겟
@@ -21,6 +22,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new WebpackPwaManifest({
+      name: "리액트 메모",
+      short_name: "메모",
+      description: "리액트로 제작한 메모",
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      theme_color: '#eeeeee',
+      icons: [
+        {
+          src: path.resolve('src/assets/bird.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ]
 };
