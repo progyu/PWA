22ba200a204fec77ca248441430b9c6da67448ba
progyu <rocket7101@gmail.com>
2019-12-21 16:11:41 +0900

    PwA

--- 191221_PWA_AZURE/src/index.js
+++ 191221_PWA_AZURE/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => (
-  <div className="app">HELLO Azure world</div>
-);
+const App = () => <div className="app">HELLO Azure world</div>;
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+// 서비스워커를 지원한다면
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js");
+  });
+}
